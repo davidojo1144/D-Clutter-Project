d6890b238835ef84b9f03906be012bf14199749b
davidojo1144 <davco1144@gmail.com>
2025-01-23 02:50:28 +0100

    D Clutter Project Using React Js || Tailwind Css

---
+++ DCLUTTER/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Buy from './pages/Buy'
+import Sell from './pages/Sell'
+import Cart from './pages/Cart'
+import Login from './pages/Login'
+import AllProducts from './pages/AllProducts'
+import Product from './pages/Product'
+import ContactUs from './pages/ContactUs'
+import Orders from './pages/Orders'
+import Search from './components/Search'
+
+
+const App = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/buy" element={<Buy/>} />
+          <Route path="/sell" element={<Sell/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="allproducts" element={<AllProducts/>} />
+          <Route path="/product" element={<Product/>} />
+          <Route path="/contactus" element={<ContactUs/>} />
+          <Route path="/order" element={<Orders/>} />
+          <Route path="/search" element={<Search/>} />
+        </Routes>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ DCLUTTER/src/assets/assets.js
@@ -0,0 +1,16 @@
+import menu from "../assets/menu.png"
+import logo from "../assets/logo.jpg"
+import search_icon from "../assets/searchicon.png"
+import cart_icon from "../assets/carticon.png"
+import cancel_icon from "../assets/cancel.jpg"
+
+
+
+
+export const assets = {
+    menu,
+    logo,
+    search_icon,
+    cart_icon,
+    cancel_icon
+}
